654478f9683722bafd71c6e1cfb7c77904541bda
LazuliKao <LazuliKao233@outlook.com>
2024-09-24 23:33:55 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,22 @@ import { FreshmanList } from "./endpoints/freshmanList";
 const app = new Hono();
 
 // Setup OpenAPI registry
+app.use("*", async (c, next) => {
+	c.res.headers.set("Access-Control-Allow-Origin", "*");
+	c.res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	c.res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	if (c.req.method === "OPTIONS") {
+		return c.newResponse("OK",
+			200,
+			{
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			}
+		);
+	}
+	await next();
+});
 const openapi = fromHono(app, {
 	docs_url: "/docs",
 });
